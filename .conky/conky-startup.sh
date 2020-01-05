34d801c063740878aa41a0efeeaaae109dfea792
sid <sid@127.0.0.1localhost>
2020-01-05 14:25:40 +0800

    删除conky的大量无用配置文件

--- .conky/conky-startup.sh
+++
@@ -1,6 +0,0 @@
-sleep 20s
-killall conky
-cd "/home/sid/.conky/Green Apple Desktop"
-conky -c "/home/sid/.conky/Green Apple Desktop/Gotham" &
-cd "/home/sid/.conky/Green Apple Desktop"
-conky -c "/home/sid/.conky/Green Apple Desktop/conky_seamod" &
